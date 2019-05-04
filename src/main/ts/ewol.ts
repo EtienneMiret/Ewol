be619bbd57894050fb4afcccced23cfa0daac1fb
Etienne Miret <etienne.miret@ens-lyon.org>
2019-05-04 17:40:26 +0200

    Draw cylinder.

--- src/main/ts/ewol.ts
+++ src/main/ts/ewol.ts
@@ -30,14 +30,41 @@ function init () {
     return;
   }
 
-  const buffer = gl.createBuffer ();
-  gl.bindBuffer (gl.ARRAY_BUFFER, buffer);
-  gl.bufferData (gl.ARRAY_BUFFER, new Float32Array ([
-    -0.5, 0, 0,
-    0.5, 0, 0,
-    0, 1, 0
-  ]), gl.STATIC_DRAW);
+  const vertices = new Float32Array (6 * PRECISION);
+  for (let index = 0; index < PRECISION; index++) {
+    const n = 6 * index;
+    const angle = index * 2 * Math.PI / PRECISION;
+    const x = Math.cos (angle);
+    const y = Math.sin (angle);
+    vertices[n] = x;
+    vertices[n + 1] = y;
+    vertices[n + 2] = -1;
+    vertices[n + 3] = x;
+    vertices[n + 4] = y;
+    vertices[n + 5] = 1;
+  }
+
+  const verticesBuffer = gl.createBuffer ();
+  gl.bindBuffer (gl.ARRAY_BUFFER, verticesBuffer);
+  gl.bufferData (gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
   gl.bindBuffer (gl.ARRAY_BUFFER, null);
+
+  const indices = new Uint16Array (6 * PRECISION);
+  for (let index = 0; index < PRECISION; index++) {
+    const n = 6 * index;
+    const m = 2 * index;
+    indices[n] = m;
+    indices[n + 1] = m + 1;
+    indices[n + 2] = m + 1;
+    indices[n + 3] = (m + 3) % (2 * PRECISION);
+    indices[n + 4] = (m + 2 * PRECISION - 2) % (2 * PRECISION);
+    indices[n + 5] = m;
+  }
+
+  const indicesBuffer = gl.createBuffer ();
+  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
+  gl.bufferData (gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, null);
 
   const program = gl.createProgram ()!;
   gl.attachShader (program, createVertexShader (gl));
@@ -45,7 +72,8 @@ function init () {
   gl.linkProgram (program);
   gl.useProgram (program);
 
-  gl.bindBuffer (gl.ARRAY_BUFFER, buffer);
+  gl.bindBuffer (gl.ARRAY_BUFFER, verticesBuffer);
+  gl.bindBuffer (gl.ELEMENT_ARRAY_BUFFER, indicesBuffer);
   const coordinates = gl.getAttribLocation (program, 'coordinates');
   gl.vertexAttribPointer (coordinates, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray (coordinates);
@@ -54,7 +82,7 @@ function init () {
   gl.enable (gl.DEPTH_TEST);
   gl.clear (gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.viewport (0, 0, canvas.width, canvas.height);
-  gl.drawArrays (gl.TRIANGLES, 0, 3);
+  gl.drawElements (gl.LINES, indices.length, gl.UNSIGNED_SHORT, 0);
   gl.finish ();
   gl.flush ();
 }
