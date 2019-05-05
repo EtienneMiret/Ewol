42bfb0f64362ba343628624809581579ff1a3398
Etienne Miret <etienne.miret@ens-lyon.org>
2019-05-05 20:04:50 +0200

    Add tiles to WorlMap object.

--- src/main/ts/load-map.ts
+++ src/main/ts/load-map.ts
@@ -10,8 +10,95 @@ export interface Wall {
   dir: Direction
 }
 
-export interface WorldMap {
+interface RawMap {
   walls: Wall[];
+}
+
+export class Tile {
+  forward = true;
+  backward = true;
+  right = true;
+  left = true;
+
+  constructor (
+      private readonly x: number,
+      private readonly y: number,
+      private readonly z: number
+  ) {
+  }
+
+  register (wall: Wall): void {
+    if (wall.z !== this.z) {
+      return;
+    }
+    switch (wall.dir) {
+      case Direction.X:
+        if (wall.x !== this.x) {
+          return;
+        }
+        this.forward = this.forward && wall.y !== this.y + 1;
+        this.backward = this.backward && wall.y !== this.y;
+        break;
+      case Direction.Y:
+        if (wall.y !== this.y) {
+          return;
+        }
+        this.right = this.right && wall.x !== this.x + 1;
+        this.left = this.left && wall.x !== this.x;
+        break;
+    }
+  }
+}
+
+export class WorldMap {
+  readonly walls: Wall[];
+  readonly tiles: Tile[][][];
+
+  constructor (map: RawMap) {
+    this.walls = map.walls;
+
+    let minX = 0;
+    let maxX = 0;
+    let minY = 0;
+    let maxY = 0;
+    let minZ = 0;
+    let maxZ = 0;
+
+    this.walls.forEach (wall => {
+      minX = Math.min (minX, wall.x);
+      maxX = Math.max (maxX, wall.x + (wall.dir === Direction.X ? 1 : 0));
+      minY = Math.min (minY, wall.y);
+      maxY = Math.max (maxY, wall.y + (wall.dir === Direction.Y ? 1 : 0));
+      minZ = Math.min (minZ, wall.z);
+      maxZ = Math.max (maxZ, wall.z + 1);
+    });
+
+    this.tiles = [];
+    for (let x = minX; x < maxX; x++) {
+      this.tiles[x] = [];
+      for (let y = minY; y < maxY; y++) {
+        this.tiles[x][y] = [];
+        for (let z = minZ; z < maxZ; z++) {
+          this.tiles[x][y][z] = new Tile (x, y, z);
+        }
+      }
+    }
+
+    this.walls.forEach (wall => {
+      const {x, y, z} = wall;
+      if (this.tiles[x]) {
+        if (this.tiles[x][y]) {
+          this.tiles[x][y][z].register (wall);
+        }
+        if (this.tiles[x][y - 1]) {
+          this.tiles[x][y - 1][z].register (wall);
+        }
+      }
+      if (this.tiles[x - 1] && this.tiles[x - 1][y]) {
+        return this.tiles[x - 1][y][z].register (wall);
+      }
+    });
+  }
 }
 
 const PASSIVE: AddEventListenerOptions = {
@@ -23,8 +110,8 @@ export function loadMap (successCallback: (m: WorldMap) => void): void {
   const xhr = new XMLHttpRequest ();
   xhr.addEventListener('load', () => {
     if (xhr.status === 200) {
-      const map: WorldMap = JSON.parse(xhr.response);
-      successCallback (map);
+      const map: RawMap = JSON.parse (xhr.response);
+      successCallback (new WorldMap (map));
     }
   }, PASSIVE);
   xhr.open ('GET', 'map');
---
+++ src/test/ts/load-mapSpec.ts
@@ -0,0 +1,38 @@
+import { Direction, WorldMap } from '../../main/ts/load-map';
+
+describe ('load-map', () => {
+
+  describe ('WorldMap', () => {
+
+    describe ('constructor', () => {
+
+      it ('should create empty map', () => {
+        const map = new WorldMap({
+          walls: []
+        });
+
+        expect (map.walls.length).toBe (0);
+        expect (map.tiles.length).toBe (0);
+      });
+
+      it ('should create single square map', () => {
+        const map = new WorldMap({
+          walls: [
+            {x: 0, y: 0, z: 0, dir: Direction.X},
+            {x: 0, y: 0, z: 0, dir: Direction.Y},
+            {x: 1, y: 0, z: 0, dir: Direction.Y}
+          ]
+        });
+
+        expect (map.walls.length).toBe (3);
+        expect (map.tiles[0][0][0].forward).toBeTruthy ();
+        expect (map.tiles[0][0][0].backward).toBeFalsy ();
+        expect (map.tiles[0][0][0].right).toBeFalsy ();
+        expect (map.tiles[0][0][0].left).toBeFalsy ();
+      });
+
+    });
+
+  });
+
+});
